48a33638b5773d829995de22797c34898486ff6e
Dusty Brigsby <dustybrigsby@gmail.com>
2023-07-31 18:21:28 -0700

    Updated models
    Defined columns for Category model
    Defined columns for Product model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,31 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  res.json({ message: 'success' });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  res.json({ message: 'success' });
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  res.json({ message: 'success' });
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  res.json({ message: 'success' });
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  res.json({ message: 'success' });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,28 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  res.json({ message: 'success' });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  res.json({ message: 'success' });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  res.json({ message: 'success' });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  res.json({ message: 'success' });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  res.json({ message: 'success' });
 });
 
 module.exports = router;
