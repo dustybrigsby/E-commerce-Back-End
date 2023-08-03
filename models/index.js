a6cf8af0810bf9ea79ae2da8767f26f9cbc11a6c
Dusty Brigsby <dustybrigsby@gmail.com>
2023-08-03 15:03:31 -0700

    Added to models
    Connected models in index.js
    defined the columns for the product tag model

--- models/index.js
+++ models/index.js
@@ -6,14 +6,25 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ''
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'product_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  foreignKey: 'tag_id',
+  through: ProductTag,
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foreignKey: 'product_id',
+  through: ProductTag,
+})
 
 module.exports = {
   Product,
